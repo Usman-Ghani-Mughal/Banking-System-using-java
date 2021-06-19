e9d3a3eab3273475e905d743e329d9f11689d036
Usman-Ghani-Mughal <usmangm087@gmail.com>
2021-06-19 08:14:38 +0500

    version 1.0

---
+++ src/bankproject/BankProject.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankproject;
+
+/**
+ *
+ * @author Usman_Ghani_Mughal
+ */
+public class BankProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
